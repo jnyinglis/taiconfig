c8cfce017e957cebc63b941a74116de98300e8e0
jnyinglis <john.inglis@me.com>
2020-10-25 11:35:20 -0400

    First test build for GitHub Pages

---
+++ docs/precache-manifest.6eae7c0c21cd433cdf900cf2d3ea9bd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "c85a8df72ed8401ba2c09bfce0f7683c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "faa73862536a3b539169",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "faa73862536a3b539169",
+    "url": "/static/js/main.faa73862.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "bb36eb2f1e66572755c6",
+    "url": "/static/js/vendors~main.bb36eb2f.chunk.js"
+  }
+]);
